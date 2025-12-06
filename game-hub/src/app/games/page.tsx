1dfda75b87f7d81b8ad750f0dbda94239a015166
Shane King <shane.m.king94@gmail.com>
2025-12-05 20:50:13 -0500

    next 16.0.1 to 16.0.7

--- game-hub/src/app/games/page.tsx
+++ game-hub/src/app/games/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { apiRequest } from "@/helpers/apiRequest";
 import toast from "react-hot-toast";
 import GameSearch from "../components/GameSearch";
